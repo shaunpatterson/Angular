a917964458efd0b60fb1f34f1e839e69711b9461
Shaun Patterson <shaunpatterson@gmail.com>
2015-09-22 20:10:41 -0400

    Step 2 Complete.  Simple unordered listing of style number and product name

--- src/app/products/products.controller.js
+++ src/app/products/products.controller.js
@@ -6,10 +6,26 @@
         .controller('ProductsController', ProductsController);
 
     /** @ngInject */
-    function ProductsController() {
+    function ProductsController($q, ProductsService, $log) {
         var vm = this;
 
         vm.title = "Angular Developer Interview Skeleton";
         vm.subtitle = "Implement your products list on this page...";
+
+        activate();
+
+        function activate() {
+            var promises = [getProducts()];
+            return $q.all(promises).then(function() { 
+                $log.info("Products loaded");
+            });
+        }
+
+        function getProducts() {
+            return ProductsService.getProducts().then(function(data) {
+                vm.products = data;
+                return vm.products;
+            });
+        }
     }
 })();
